c2c66241c2ca8b21cbfddd9be9f5fa741b9502bc
BlueAeon <BlueAeon@users.noreply.github.com>
2015-03-20 17:34:58 -0400

    Writing tdee.py's avg() to use nested dictionaries

--- tdee.py
+++ tdee.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python
 import sys
 
-sys.setrecursionlimit(10000)    
-max = 50 # max depth for recursive functions                              
+# Shamelessly ripped from stackoverflow.com
+class Vivify(dict):
+    """Implementation of perl's autovivification feature."""
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
 
-if len(sys.argv) < 2:
-    sys.exit('Usage: ./tdee.py <days> <logfile>')
+def avg(input, result, smooth):
+    for i in range(len(input)):
+        if i == 0:
+            result[i] = input[i]
+        else:
+            result.append(result[i-1] + (1 - smooth)*(input[i] - result[i-1]))
 
-# the average of a given day Asub(d) 
-# the measurement of a day Msub(d)
-# The smoothing constant S (eg: .9) 
-# The smoothing Percentage P (1 - S) 
-# Asub(d) = Asub(d-1) + P * (Msub(d) - Asub(d-1))
+def main():
+    if len(sys.argv) < 2:
+        sys.exit('Usage: ./tdee.py <logfile>')
+    data = Vivify()
+    f = open(sys.argv[1],'r')
+    for line in f.read().splitlines():
+        word = line.split(' ')
+        data[word[0]]['weight'] = word[1] 
+        data[word[0]]['intake'] = word[2] 
+    print(data)
+    # take all the weights and put them into an array
+    # calculate average
+    # put the averages into matching dates
+    print(len(data))
+    weight = [190,190,180,190,185,180,185]
+    average = [None]
+    smooth = .9
+    avg(weight, average, smooth) 
 
-# Curved moving averages
-# prepare for brain-assault
-## term: Array of values to average
-## depth: Maximum depth to recurse
-## smooth: Smoothing constant
-## maxdepth: Maximum depth to step backwards into list
-# average = smooth * average + (1 - smooth) * term
-def avg(term, depth, smooth, maxdepth):
-    average = term[depth]
-    print(term[depth],depth,len(term)-depth,maxdepth)
-    if depth > 0 and len(term)-depth < maxdepth: 
-        # at [1] this returns M[0]
-        average = avg(term, depth-1, smooth, maxdepth)
-    return average
-
-weights = [190,190,180,190,185,180,185]
-ndays = len(weights) - 1
-if ndays > max:
-    ndays = max
-#avg(weights, ndays, .9, max) 
-
-average = weights[0]
-smooth = .9
-for weight in weights:
-    average = 
-    print(average)
-
-# start with an empty array of averages the same length of weights
-# set average[0] to weight[0]
-# average[i] = average[i-1] + (1 - smooth)*(weight[i] - average[i-1)
+if __name__ == "__main__":
+    main()
