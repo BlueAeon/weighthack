f47dafaf5b6c4ab744b3473cd08f2b69e7700a99
BlueAeon <BlueAeon@users.noreply.github.com>
2015-03-22 10:28:53 -0400

    Added DataMap __str__ and parseFile()

--- DataMap.py
+++ DataMap.py
@@ -2,12 +2,13 @@
 from datetime import date, timedelta
 
 # TODO:
-## implement average() and parseFile() and plot()
+## implement average() and plot()
 class DataMap(dict):
     '''Base class is a perl-like autovivification object with built in vars
         for metrics that will be tracked. DataMap.day should be used for
         any date offset calculations'''
     pointcount = 0 
+
     def __init__(self, datestr="1987-11-02", weight=0, intake=0, avg=0, tdee=0):
         DataMap.pointcount += 1
         self.weight = weight
@@ -16,12 +17,33 @@ class DataMap(dict):
                             int(datestr.split('-')[2])) #clean this up somehow
         self.avg = avg
         self.tdee = tdee
+
     def __getitem__(self, item):
         try:
             return dict.__getitem__(self, item)
         except KeyError:
             value = self[item] = type(self)(item)
             return value
+
+    # TODO: Set unparsed variables as NoneType
+    def parseFile(self, filestr):
+        try:
+            f = open(filestr)
+        except FileNotFoundError:
+            print("Cannot open file \"" + filestr + "\"")
+            return 
+        for line in f.read().splitlines():
+            word = line.split(' ')
+            self[word[0]].weight = word[1]
+            self[word[0]].intake = word[2]
+        f.close()
+        return DataMap.pointcount 
+         
+    def __str__(self):
+        ret = ""
+        for key in self:
+            ret += key + ": " + str(self[key].weight) + " " + str(self[key].intake) + "\n"
+        return ret
 
 # Just for debugging
 def main():
@@ -35,11 +57,12 @@ def main():
     data['2015-03-16'].weight = 180
     data['2015-03-16'].intake = 1004
     
-    print(data)
-    for key in data:
-        print(data[key].weight)
-
     print(data['2015-03-16'].day)
+    data.parseFile("sample_data")
+
+    print(data)
+    print(data['2015-03-02'].weight)
+    print(data['2015-03-02'].intake)
 
 if __name__ == "__main__":
     main()
